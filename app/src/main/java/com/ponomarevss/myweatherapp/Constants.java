2582fd6101f05a1464ae719cb30d2082e2396a25
ponomarevss <s-siberia@yandex.ru>
2020-04-17 14:58:53 +0500

    initial commit

---
+++ app/src/main/java/com/ponomarevss/myweatherapp/Constants.java
@@ -0,0 +1,8 @@
+package com.ponomarevss.myweatherapp;
+
+public interface Constants {
+    String PLACE = "PLACE";
+    String PARCEL = "PARCEL";
+    String SET_PLACE = "set place";
+    int INIT_INDEX = -1;
+}
